d84d0bda874d23223c62ec8284f1240b5d311053
lunky <quinn@midworld.org>
2023-12-25 14:38:07 -0600

    refactor day11

--- src/Day11.hs
+++ src/Day11.hs
@@ -8,21 +8,20 @@ module Day11
 import Data.List
 
 day11 :: String -> Int
-day11 input = sum $ map distance $ uniquePairs $ parseGalaxies' (lines input) $ parseInput' 2 input
+day11 input = sum $ map distance $ uniquePairs $ parseGalaxies $ parseInput 2 input
 
+day11b :: String -> Int -> Int
+day11b input multiplier = sum $ map distance $ uniquePairs $ parseGalaxies $ parseInput multiplier input
 
+distance :: ((Int,Int), (Int,Int)) -> Int
 distance ((x1,y1), (x2,y2))
    = (max x2 x1 - min x1 x2 ) +  (max y2 y1 - min y1 y2 )
 
+uniquePairs :: Ord b => [b] -> [(b,b)]
 uniquePairs l = [(x,y) | x <- l, y <- l, x < y]
 
-day11b :: String -> Int -> Int
-day11b input multiplier = sum $ map distance $ uniquePairs $ parseGalaxies' (lines input) $ parseInput' multiplier input
-
-
-
-
-parseInput' z input = (xaxis,yaxis)
+parseInput :: Int -> String -> ([String],[Int],[Int])
+parseInput z input = (ys,xaxis,yaxis)
   where yaxis = getIndexes z ys
         xaxis = getIndexes z $ transpose ys
         ys = lines input
@@ -31,7 +30,8 @@ parseInput' z input = (xaxis,yaxis)
                then (x+z):acc
                else (x+1):acc ) [0] ys
 
-parseGalaxies' xs (xaxis,yaxis) =  map fst
+parseGalaxies :: ([String], [a],[b]) -> [(a,b)]
+parseGalaxies (xs, xaxis,yaxis) =  map fst
                      $ filter (\(_,y)->y=='#')
                      $ concatMap (\(y,xs)->zipWith (\ x z -> ((x, y), z)) xaxis xs)
                      $ zip yaxis xs
